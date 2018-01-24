852365f7e71a330da6e2335e44c9da6230c82682
Agnes Chang <ac3882@columbia.edu>
2018-01-24 01:05:39 -0500

    Add time to A1 Helloworld

--- A1/sketch.js
+++ A1/sketch.js
@@ -3,14 +3,22 @@ function setup() {
 }
 
 function draw() {
-  // see https://p5js.org/reference/#/p5/background
   background('green');
-  // see https://p5js.org/reference/#/p5/fill
   fill(255, 204, 0);
-  rect(20, 20, 60, 60);
-  // see https://p5js.org/reference/#/p5/ellipse
+
+  // Map the function second() to values from 0~400
+  rect(20, 20, 60, map(second(), 0, 60, 0, 400));
+
+  push(); // Start a new drawing state
+  scale(11, 11); // Use function hour() directly to apply scale
   ellipse(56, 46, 55, 55);
-  // see https://p5js.org/reference/#/p5/text
+  pop(); // Restore original state (scale, specifically)
+
+
+  // First change the mode to degrees (default is radians)
+  angleMode(DEGREES);
+  // Map the function minute() to values from 0~360
+  rotate(map(minute(), 0, 60, 0, 360));
   textSize(32);
   fill(0, 102, 153);
   text('Agnes says hi', 10, 60);
